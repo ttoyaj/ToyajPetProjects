a9d27ef45e855f2ab249882e4584168c8bd9f05a
Toyaj <ttoyaj@gmail.com>
2025-09-21 18:51:39 +0530

    Sept 22

--- Ping_pong/main.go
+++ Ping_pong/main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func main() {
-	// Serve static files (index.html will be the root page)
+// letReplyToPing decides response based on current IST time
+func letReplyToPing() string {
+	// Load IST timezone
+	ist, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		return "Error loading timezone"
+	}
 
+	now := time.Now().In(ist)
+	hour := now.Hour()
+
+	if hour < 21 {
+		return "PONG from Toyaj"
+	} else {
+		return "PONG from Adithi"
+	}
+}
+
+func main() {
 	// Serve static files from /static/
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// Serve index.html at root
+	// Serve static/index.html at root
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, "static/index.html")
 	})
 
-	// API endpoint
+	// API endpoint with time-based response
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "pong")
+		reply := letReplyToPing()
+		fmt.Fprintln(w, reply)
 	})
 
 	fmt.Println("Server running at http://localhost:8080")
